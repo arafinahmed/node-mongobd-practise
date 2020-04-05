b3fffbe93650bdfa2dd55dcaa87903094bef34f0
arafinahmed <arafin2021@gmail.com>
2020-04-06 05:56:03 +0600

    api added

--- index.js
+++ index.js
@@ -38,6 +38,26 @@ app.get('/products', (req, res) =>{
       });
 });
 
+app.get('/orders', (req, res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        
+        const collection = client.db("test").collection("orders");
+        collection.find().toArray((err, documents)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+      });
+});
+
+
+
 app.get('/product/:key', (req, res) =>{
     const key = req.params.key;  
     
